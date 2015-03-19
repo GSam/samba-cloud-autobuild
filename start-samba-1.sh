6735cff9c5d911832a967152f9e91bf0a9501b2f
Andrew Bartlett <abartlet@samba.org>
2015-03-19 18:53:38 +1300

    Boot a bigger machine with more memory and CPU
    Signed-off-by: Andrew Bartlett <abartlet@samba.org>

--- start-samba-1.sh
+++ start-samba-1.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 . ~/sambatest.catalyst.net.nz-openrc.sh
-nova boot --flavor=c093745c-a6c7-4792-9f3d-085e7782eca6 --image=ubuntu-14.04-x86_64 --user-data user-config.yaml --config-drive true --key-name=abartlet --poll samba-build-$1
+#c1.c2r4 c093745c-a6c7-4792-9f3d-085e7782eca6
+#c1.c4r8 45060aa3-3400-4da0-bd9d-9559e172f678
+nova boot --flavor=45060aa3-3400-4da0-bd9d-9559e172f678 --image=ubuntu-14.04-x86_64 --user-data user-config.yaml --config-drive true --key-name=abartlet --poll samba-build-$1
