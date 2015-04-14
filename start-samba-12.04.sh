21d8d5d49580f0df2856ec75f1e3237ce15cfff3
Andrew Bartlett <abartlet@samba.org>
2015-04-14 12:28:58 +1200

    Use template images that have the apt-get update and git clone already done
    Signed-off-by: Andrew Bartlett <abartlet@samba.org>

--- start-samba-12.04.sh
+++ start-samba-12.04.sh
@@ -3,4 +3,4 @@
 #c1.c2r4 c093745c-a6c7-4792-9f3d-085e7782eca6
 #c1.c4r8 45060aa3-3400-4da0-bd9d-9559e172f678
 #c1.c4r4 62473bef-f73b-4265-a136-e3ae87e7f1e2
-nova boot --flavor=62473bef-f73b-4265-a136-e3ae87e7f1e2 --image=ubuntu-12.04-x86_64 --user-data user-config-12.04.yaml --config-drive true --key-name=abartlet --poll samba-build-$1
+nova boot --flavor=62473bef-f73b-4265-a136-e3ae87e7f1e2 --image=samba-build-12.04-template --user-data user-config-12.04.yaml --config-drive true --key-name=abartlet --poll samba-build-$1
