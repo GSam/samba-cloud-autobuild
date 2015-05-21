b38884a76b69881dde8f0fbe51dd8c7bebda4dad
Douglas Bagnall <douglas.bagnall@catalyst.net.nz>
2015-05-21 13:59:34 +1200

    Remove obsolete stop scripts

--- stop-samba-1-wlg.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-. ~/sambatest.catalyst.net.nz-openrc-wlg.sh
-nova delete samba-build-$1
-
--- stop-samba-1.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-. ~/sambatest.catalyst.net.nz-openrc.sh
-nova delete samba-build-$1
-
