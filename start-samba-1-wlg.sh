83394f99884043e5fc265f803a85d2a1ca530069
Douglas Bagnall <douglas.bagnall@catalyst.net.nz>
2015-05-21 13:55:21 +1200

    Remove obsolete start scripts and yaml files
    These have hard-coded branches etc.

--- start-samba-1-wlg.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-. ~/sambatest.catalyst.net.nz-openrc-wlg.sh
-nova boot --flavor=c1.c2r2 --image=ubuntu-14.04-x86_64 --user-data user-config.yaml --config-drive true --key-name=abartlet --poll samba-build-$1
--- start-samba-12.04-wlg.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-. ~/sambatest.catalyst.net.nz-openrc-wlg.sh
-#c1.c4r4 2772d378-c1c3-464c-83b9-1a2d00d1b66b
-nova boot --flavor=2772d378-c1c3-464c-83b9-1a2d00d1b66b --image=ubuntu-12.04-x86_64 --user-data user-config-12.04.yaml --config-drive true --key-name=abartlet --poll samba-build-$1
