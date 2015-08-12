20d3130f8e415a32863fe085fa3fca488c363ec7
Douglas Bagnall <douglas.bagnall@catalyst.net.nz>
2015-08-13 10:10:37 +1200

    Increase default timeout, because default is now 2 CPUs
    Signed-off-by: Douglas Bagnall <douglas.bagnall@catalyst.net.nz>

--- common.py
+++ common.py
@@ -112,7 +112,7 @@ def add_common_args(parser):
     parser.add_argument('--readahead', default=8192, type=int,
                         help=("block device readahead (default 8192)"))
 
-    parser.add_argument('--maxtime', default=10000, type=int,
+    parser.add_argument('--maxtime', default=12000, type=int,
                         help=("how long to wait before timing out"))
 
     parser.add_argument('--tmpfs', action='store_true',
