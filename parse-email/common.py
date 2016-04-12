c412e6cfbc67a0ed6e99dc446fae8bf234ada600
Douglas Bagnall <douglas.bagnall@catalyst.net.nz>
2016-04-12 13:57:53 +1200

    parse-email: add a script for parsing Catalyst cloud email
    The main difference is these attach a tarball rather than sending
    links.
    
    Common code was moved into common.py.
    
    Signed-off-by: Douglas Bagnall <douglas.bagnall@catalyst.net.nz>

---
+++ parse-email/common.py
@@ -0,0 +1,51 @@
+import os
+import sys
+import mailbox
+import argparse
+import re
+import requests
+import errno
+import collections
+import time
+
+import config
+
+
+def filter_by_date(files, date):
+    date_search = re.compile(r'(20\d\d-\d\d-\d\d)-').search
+    out = []
+    threshold = time.mktime(time.strptime(date, '%Y-%m-%d'))
+    for fn in files:
+        m = date_search(fn)
+        if m:
+            t = time.mktime(time.strptime(m.group(1), '%Y-%m-%d'))
+            if t > threshold:
+                out.append(fn)
+    return out
+
+
+def count_lines(fn_re, line_re, cache, count=None, since=None):
+    fn_match = re.compile(fn_re).search
+    line_match = re.compile(line_re).search
+    files = sorted(x for x in os.listdir(cache) if fn_match(x))
+    if since:
+        files = filter_by_date(files, since)
+
+    lines = []
+    for fn in files:
+        f = open(os.path.join(cache, fn))
+        for line in f:
+            if line_match(line):
+                lines.append(line.strip())
+        f.close()
+
+    print ("found %d lines matching %r in %d files matching %r" %
+           (len(lines), line_re, len(files), fn_re))
+
+    c = collections.Counter(lines)
+    if count:
+        rows = c.most_common(count)
+    else:
+        rows = c.most_common()
+    for k, v in rows:
+        print '%4d %s' % (v, k)
