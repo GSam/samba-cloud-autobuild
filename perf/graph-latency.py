54fe892378bc87bb24f2dca90eca0442222056c3
Douglas Bagnall <douglas.bagnall@catalyst.net.nz>
2016-07-13 10:04:40 +1200

    perf/graph-latency: whitespace

--- perf/graph-latency.py
+++ perf/graph-latency.py
@@ -8,12 +8,12 @@ from math import exp
 def plot_n_squared(ax, n, scale):
     seq = [(i * scale) ** 2 for i in range(n) ]
     ax.plot(seq, 'k-', linewidth=1, label='(n * %g) ^ 2' % scale)
-    
+
 
 def plot_exp_n(ax, n, scale):
     seq = [exp(i * scale)for i in range(n) ]
     ax.plot(seq, 'k:', linewidth=1, label='exp(n * %g)' % scale)
-    
+
 
 def plot(sequences, keys):
     ax = plt.subplot(1, 1, 0)
@@ -21,7 +21,7 @@ def plot(sequences, keys):
     for c, s, k in zip('rgby', sequences, keys):
         ax.plot(s, '%s.' % c, linewidth=0.5, label=k)
     return ax
-    
+
 def main():
     keys=None
     rows = []
@@ -43,8 +43,8 @@ def main():
     #plot_exp_n(ax, len(rows), 5e-5)
 
     plt.grid(True)
-    plt.legend(loc='upper left', numpoints=1, frameon=False, borderpad=0)    
+    plt.legend(loc='upper left', numpoints=1, frameon=False, borderpad=0)
     plt.show()
-    
+
 
 main()
