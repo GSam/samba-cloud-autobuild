15ec7f1c67a060927b8defb78bde37fa1ec78d44
Douglas Bagnall <douglas.bagnall@catalyst.net.nz>
2017-12-28 11:11:28 +1300

    parse-autobuild-email: --run-time to plot time till failure

--- parse-email/email_utils.py
+++ parse-email/email_utils.py
@@ -66,14 +66,18 @@ def group_by_month(files):
     return out
 
 
-def get_errors_by_month(fn_re, line_re, cache, since=None, filter_re=None):
+def get_files_by_month(fn_re, cache, since):
     fn_match = re.compile(fn_re).search
-    line_match = re.compile(line_re).search
     files = sorted(x for x in os.listdir(cache) if fn_match(x))
     if since:
         files = filter_by_date(files, since)
-
     months = group_by_month(files)
+    return months
+
+
+def get_errors_by_month(fn_re, line_re, cache, since=None, filter_re=None):
+    line_match = re.compile(line_re).search
+    months = get_files_by_month(fn_re, cache, since)
     month_lines = []
     longest = 0
     for month, filenames in months.iteritems():
@@ -113,6 +117,75 @@ def draw_histogram(fn_re, line_re, cache, since=None, filter_re=None):
             x = x * 72 // longest
 
         print '%s %3d %s' % (month, len(lines), '#' * x)
+
+
+TIME_MATCH = r'/\d+ at (\d+h)?(\d+m)?(\d+s)\] '
+
+
+def draw_runtime_histogram(fn_re, cache, since, line_re=TIME_MATCH):
+    months = get_files_by_month(fn_re, cache, since)
+    line_match = re.compile(line_re).search
+    month_times = []
+    longest = 0
+    for month, filenames in months.iteritems():
+        times = []
+        for fn in filenames:
+            f = open(os.path.join(cache, fn))
+            lines = reversed(f.readlines())
+            for line in lines:
+                m = line_match(line)
+                if m:
+                    g = m.groups()
+                    t = 0
+                    for x in g:
+                        if x is None:
+                            continue
+                        d = int(x[:-1])
+                        u = x[-1]
+                        if u == 'h':
+                            t += d * 3600
+                        elif u == 'm':
+                            t += d * 60
+                        else:
+                            t += d
+                    times.append(t)
+                    break
+            f.close()
+
+        month_times.append((month, times))
+
+    month_times.sort()
+    longest = 1
+    for month, times in month_times:
+        times.sort()
+        longest = max(longest, times[-1])
+
+    def s(t):
+        return t * 73 // longest
+
+    r = [' '] * 75
+    for i in range(1 + longest // 3600):
+        r[s(i * 3600)] = str(i)[-1]
+
+    key = 'hours   ' + ''.join(r)
+    print key
+
+    # mark the extrema, quartiles and median
+    for month, times in month_times:
+        r = [' '] * 75
+        r[s(times[0])] = '.'
+        r[s(times[-1])] = '.'
+        q1 = s(times[len(times) // 4])
+        q3 = s(times[len(times) * 3 // 4])
+        r[q1] = '|'
+        r[q3] = '|'
+        r[q1 + 1: q3] = ['-'] * (q3 - q1 - 1)
+        r[s(times[len(times) // 2])] = '#'
+        print month, ''.join(r)
+
+    print key
+    print "key: median #   interquartile range |-----|   extrema ."
+
 
 
 def recurring_errors(fn_re, line_re, cache, since=None, filter_re=None,
