150558eb4970b62caa8124958dd3f84447064c6f
Joe Guo <joeg@catalyst.net.nz>
2018-08-14 15:29:02 +1200

    roles/samba-dc: add var samba_install_dir to support restore
    Signed-off-by: Joe Guo <joeg@catalyst.net.nz>

--- roles/samba-dc/templates/domain-restore.sh
+++ roles/samba-dc/templates/domain-restore.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 /usr/local/samba/bin/samba-tool domain backup restore \
-  --newservername={{primary_dc_name}} \
+  --newservername={{primary_dc_name}}-restore \
   --backup-file={{ansible_env.HOME}}/samba-backup \
   --targetdir={{samba_restore_target_dir}} \
   --username={{samba_username}} \
