f257ab6d7a1d7cbc1a5de2c99a4d45f52b1a34ef
Joe Guo <joeg@catalyst.net.nz>
2018-08-14 13:47:27 +1200

    roles/samba-dc: fix backup file dest
    Signed-off-by: Joe Guo <joeg@catalyst.net.nz>

--- roles/samba-dc/templates/domain-restore.sh
+++ roles/samba-dc/templates/domain-restore.sh
@@ -2,7 +2,7 @@
 
 /usr/local/samba/bin/samba-tool domain backup restore
   --newservername={{primary_dc_name}} \ \
-  --backup-file={{samba_backup_file}} \
+  --backup-file={{{{ansible_env.HOME}}/samba-backup}} \
   --targetdir={{samba_restore_target_dir}} \
   --username={{samba_username}} \
   --password={{samba_password}}
