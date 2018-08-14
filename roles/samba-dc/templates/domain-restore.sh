eff823fc0dc0ea254ee54fb104f687830585e2f2
Joe Guo <joeg@catalyst.net.nz>
2018-08-14 14:49:39 +1200

    roles/samba-dc/templates/domain-restore.sh: fix template
    Signed-off-by: Joe Guo <joeg@catalyst.net.nz>

--- roles/samba-dc/templates/domain-restore.sh
+++ roles/samba-dc/templates/domain-restore.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-/usr/local/samba/bin/samba-tool domain backup restore
-  --newservername={{primary_dc_name}} \ \
-  --backup-file={{{{ansible_env.HOME}}/samba-backup}} \
+/usr/local/samba/bin/samba-tool domain backup restore \
+  --newservername={{primary_dc_name}} \
+  --backup-file={{ansible_env.HOME}}/samba-backup \
   --targetdir={{samba_restore_target_dir}} \
   --username={{samba_username}} \
   --password={{samba_password}}
