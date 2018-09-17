996de7f3f6167b54d61dad8399399e3f3c403b53
Joe Guo <joeg@catalyst.net.nz>
2018-09-17 17:33:55 +1200

    wpts/templates/Kerberos/run.cmd: render run.cmd with vs vars
    Signed-off-by: Joe Guo <joeg@catalyst.net.nz>

--- wpts/templates/Kerberos/run.cmd
+++ wpts/templates/Kerberos/run.cmd
@@ -7,22 +7,9 @@ echo ==============================================
 echo          Start to run Kerberos all test cases
 echo ==============================================
 
-if not defined vspath (
-    if defined VS110COMNTOOLS (
-        set vspath="%VS110COMNTOOLS%"
-    ) else if defined VS120COMNTOOLS (
-        set vspath="%VS120COMNTOOLS%"
-    ) else if defined VS140COMNTOOLS (
-        set vspath="%VS140COMNTOOLS%"
-    ) else (
-        echo Error: Visual Studio or Visual Studio test agent should be installed, version 2012 or higher
-        goto :eof
-    )
-)
-
 del C:\testresult.trx
 
-%vspath%"..\IDE\mstest.exe" ^
+{{MSTEST}}" ^
 /testContainer:{{test_suite_bin}}\Kerberos_ServerTestSuite.dll ^
 /runconfig:{{test_suite_bin}}\ServerLocalTestRun.testrunconfig ^
 /usestderr /noisolation /resultsfile:C:\testresult.trx
