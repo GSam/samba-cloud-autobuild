61a6aa1e5be2164c02cc25840be0672eaa240a35
Your Name <you@example.com>
2018-09-29 09:29:45 +0000

    temporary

---
+++ wpts/templates/ADFamily/run.cmd
@@ -0,0 +1,17 @@
+:: Copyright (c) Microsoft. All rights reserved.
+:: Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+@echo off
+
+echo ==============================================
+echo          Start to run ADFamily all test cases
+echo ==============================================
+
+del {{test_result_file}} >nul 2>&1
+
+"{{MSTEST}}" ^
+/testContainer:{{test_suite_bin}}\AD_ServerTestSuite.dll ^
+/runconfig:{{test_suite_bin}}\ServerLocalTestRun.testrunconfig ^
+/resultsfile:{{test_result_file}} ^
+/usestderr ^
+/noisolation
